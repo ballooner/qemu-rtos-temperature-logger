32891da8e9ac656f18aad84dd9aba71b79d1a7e5
Jace Duley <26182964+ballooner@users.noreply.github.com>
2026-03-11 09:44:09 -0400

    Cleaned up unnecessary demo files

--- FreeRTOS/Demo/ThirdParty/Community-Supported-Demos/RH850_F1KM_S4_CCRH/bsp/bsp_irq/vector_data.h
+++
@@ -1,9 +0,0 @@
-#ifndef BSP_IRQ_VECTOR_DATA_H_
-#define BSP_IRQ_VECTOR_DATA_H_
-
-extern void vPortTickISR(void);
-
-extern void TAUD0I1_Handler(void);
-extern void TAUD0I3_Handler(void);
-
-#endif                                 /* BSP_IRQ_VECTOR_DATA_H_ */
